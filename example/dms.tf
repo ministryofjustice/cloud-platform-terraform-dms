0790329d3decffb395b84a108a44d0c58dfaddf7
Mourad T <mourad.trabelsi@digital.justice.gov.uk>
2019-05-30 15:23:19 +0100

    add example files: module invocation, provider, tfvars;

---
+++ example/dms.tf
@@ -0,0 +1,25 @@
+variable "source_database_name" {}
+
+variable source_database_username {}
+variable source_database_password {}
+variable source_database_host {}
+variable target_database_name {}
+variable target_database_username {}
+variable target_database_password {}
+variable target_database_host {}
+
+module "example_dms" {
+  source                   = ".."
+  team_name                = "example-team"
+  business-unit            = "example-bu"
+  application              = "exampleapp"
+  environment-name         = "development"
+  source_database_name     = "${var.source_database_name}"
+  source_database_username = "${var.source_database_username}"
+  source_database_password = "${var.source_database_password}"
+  source_database_host     = "${var.source_database_host}"
+  target_database_name     = "${var.target_database_name}"
+  target_database_username = "${var.target_database_username}"
+  target_database_password = "${var.target_database_password}"
+  target_database_host     = "${var.target_database_host}"
+}
