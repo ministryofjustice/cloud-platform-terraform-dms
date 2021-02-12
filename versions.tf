2eee96c586205b7e5de27572c33ea2e1ca319579
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2021-02-12 14:46:58 +0000

    Add file

---
+++ versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    local = {
+      source = "hashicorp/local"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+  required_version = ">= 0.13"
+}
