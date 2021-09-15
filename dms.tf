5c8f47d173759c8c5f3850c187ed08345262523e
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2021-09-15 09:33:58 +0100

    basic instance

--- dms.tf
+++ dms.tf
@@ -1,3 +1,21 @@
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
+data "aws_vpc" "selected" {
+  filter {
+    name   = "tag:Name"
+    values = [var.cluster_name == "live" ? "live-1" : var.cluster_name]
+  }
+}
+
+data "aws_subnet_ids" "private" {
+  vpc_id = data.aws_vpc.selected.id
+
+  tags = {
+    SubnetType = "Private"
+  }
+}
+
 resource "random_id" "id" {
   byte_length = 8
 }
@@ -5,13 +23,8 @@ resource "random_id" "id" {
 # Create a new replication subnet group
 resource "aws_dms_replication_subnet_group" "replication-subnet-group" {
   replication_subnet_group_description = "Replication subnet group for ${var.application}"
-  replication_subnet_group_id          = "dms-subnet-group-${random_id.id.hex}"
-
-  subnet_ids = [
-    "subnet-008096de384cdb660",
-    "subnet-042d27892b9d249dc",
-    "subnet-07fa62f055b2bcfce",
-  ]
+  replication_subnet_group_id          = "${var.team_name}-sg-${random_id.id.hex}"
+  subnet_ids                           = data.aws_subnet_ids.private.ids
 
   tags = {
     Name        = "${var.team_name} DMS subnet group"
@@ -24,13 +37,13 @@ resource "aws_dms_replication_subnet_group" "replication-subnet-group" {
 
 # Create a new replication instance
 resource "aws_dms_replication_instance" "replication-instance" {
-  allocated_storage           = 20
+  allocated_storage           = var.allocated_storage
   apply_immediately           = true
-  auto_minor_version_upgrade  = true
-  engine_version              = "3.1.3"
+  auto_minor_version_upgrade  = false
+  engine_version              = "3.4.5"
   publicly_accessible         = false
   replication_instance_class  = "dms.t2.medium"
-  replication_instance_id     = "dms-replication-instance-${random_id.id.hex}"
+  replication_instance_id     = "${var.team_name}-dms-${random_id.id.hex}"
   replication_subnet_group_id = aws_dms_replication_subnet_group.replication-subnet-group.id
 
   tags = {
--- variables.tf
+++ variables.tf
@@ -1,3 +1,6 @@
+variable "cluster_name" {
+}
+
 variable "team_name" {
 }
 
@@ -15,54 +18,17 @@ variable "business-unit" {
 variable "namespace" {
 }
 
-variable "engine_type" {
-  description = "Engine used e.g. postgres"
-  default     = "postgres"
-}
-
-variable "engine_version" {
-  description = "The engine version to use e.g. 9.6"
-  default     = "9.6"
-}
-
 variable "instance_type" {
   description = "replication instance size, e.g dms.t2.medium"
   default     = "dms.t2.medium"
 }
 
+variable "allocated_storage" {
+  description = "how many GB for local buffer"
+  default     = 32
+}
+
 variable "aws_region" {
   description = "Region into which the resource will be created."
   default     = "eu-west-2"
-}
-
-variable "source_database_name" {
-  description = "Name of source database (psql -d)"
-}
-
-variable "source_database_username" {
-  description = "username in source database (psql -U)"
-}
-
-variable "source_database_password" {
-  description = "user's password in source database"
-}
-
-variable "source_database_host" {
-  description = "host, endpoint of source database (psql -h)"
-}
-
-variable "target_database_name" {
-  description = "Name of target database (psql -d)"
-}
-
-variable "target_database_username" {
-  description = "username in target database (psql -U)"
-}
-
-variable "target_database_password" {
-  description = "user's password in target database (psql -d)"
-}
-
-variable "target_database_host" {
-  description = "host, endpoint of target database (psql -d)"
 }
